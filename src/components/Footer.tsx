8b2d3e4ac111bcebe6bca24d09b41983572e5c56
devwillians <vwillians22@gmail.com>
2024-03-20 15:54:23 -0500

    cargando proyecto

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer style={{ backgroundColor: '#2DD4BF', color: '#fff', textAlign: 'center', padding: '20px' }}>
+      <p>
+        Joosudev - Este proyecto está bajo la Licencia MIT &copy; {currentYear}
+      </p>
+      <a href="https://github.com/joosudev/controlgastos.git" target="_blank" rel="noopener noreferrer">
+        <img src="https://img.icons8.com/material-rounded/48/ffffff/github.png" alt="GitHub" style={{ margin: 'auto', display: 'block', width: '48px', height: '48px' }} />
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
