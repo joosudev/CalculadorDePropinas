28a08c2449bf85414919d447e844186661620589
VladimirWillians <47118243+joosudev@users.noreply.github.com>
2024-03-28 15:19:40 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
       <p>
         Joosudev - Este proyecto está bajo la Licencia MIT &copy; {currentYear}
       </p>
-      <a href="https://github.com/joosudev/controlgastos.git" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/joosudev/CalculadorDePropinas.git" target="_blank" rel="noopener noreferrer">
         <img src="https://img.icons8.com/material-rounded/48/ffffff/github.png" alt="GitHub" style={{ margin: 'auto', display: 'block', width: '48px', height: '48px' }} />
       </a>
     </footer>
